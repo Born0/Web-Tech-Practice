ad8d5a7cb5402cb74cc0cf8c6862d331e6c482e6
Md.Sirajuddin Borno <40140598+Born0@users.noreply.github.com>
2020-10-19 19:33:03 +0600

    into to PHP

---
+++ First.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>HTML</head><br><br>
+<body>
+
+</body>
+</html>
+
+<?php
+   for($x=11; $x<=100; $x=$x+2)
+   {
+       echo $x."<br>";  
+   }
+
+?>
--- loginAction/Result.php
+++ loginAction/Result.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Log in Successful !</title>
     <h1>login Success</h1>
+    <a href="../Registration.php"> back </a>
 </head>
 <body>
     
