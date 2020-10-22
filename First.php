06ca99d825cdefca0c94ca9d5ad1bc50e839dd1b
Md.Sirajuddin Borno <40140598+Born0@users.noreply.github.com>
2020-10-22 09:37:08 +0600

    First.php (updated )

--- First.php
+++ First.php
@@ -1,15 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>HTML</head><br><br>
+    <head></head><br><br>
 <body>
 
 </body>
 </html>
 
 <?php
-   for($x=11; $x<=100; $x=$x+2)
-   {
-       echo $x."<br>";  
-   }
+class MyCircle
+{
+    private $radius;
+    function MyCircle($radius)
+    {
+       $this->radius=$radius;
+       echo "Initialize<br><br>";
+    }
+    function __destruct(){
+        echo "<br><br>Destroy";
+    }
+    function setRadius($radius)
+    {
+        $this->radius=$radius;
+    }
 
+    function getRadius()
+    {
+        return $this->radius;
+    }
+    function getArea()
+    {
+        $area=(3.1416*$this->radius*$this->radius);
+        return $area;
+    }
+}
+
+    $circle = new MyCircle(3);
+    echo "Radius from Constructor: ";
+    echo $circle->getRadius();
+    $circle->setRadius(5);
+    echo "<br>Radius after set: ";
+    echo $circle->getRadius();
+    echo "<br>Area: ";
+    echo $circle->getArea();
 ?>
